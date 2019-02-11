055b2a7ae98b5f47b18985bae9abbc2423a89647
Ewa Malusecka <maluseckae@gmail.com>
2019-02-11 07:59:54 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,42 @@
 // Code Product Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+export default class Product extends React.Component {
+  render() {
+    return(
+      {this.props.name}
+      {this.props.producer}
+      {this.props.hasWatermark}
+      {this.props.color}
+      {this.props.weight}
+    )
+  }
+}
+
+Order.defaultProps = {
+  hasWatermark: false
+}
+
+Order.PropTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.string.isRequired.oneOf(['white','eggshell-white','salmon'])
+  weight: PropTypes.number.isRequired.range
+};
+
+function range(props, propName, componentName) {
+  componentName = comopnentName || 'ANONYMOUS';
+
+  if (props[propName]) {
+    let value = props[propName];
+    if (typeof value === 'number') {
+        return (value >= 80 && value <= 300) ? null : new Error(propName + ' in ' + componentName + " is not within 80 to 300");
+    }
+  }
+
+  // assume all ok
+  return null;
+}
